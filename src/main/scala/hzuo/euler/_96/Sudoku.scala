bbce2d220a816e9dbe0ce2b3459677ca09fac123
hzuo <someotherusername@gmail.com>
2013-12-03 10:51:28 -0500

    cleaned up 96

--- src/main/scala/hzuo/euler/_96/Sudoku.scala
+++ src/main/scala/hzuo/euler/_96/Sudoku.scala
@@ -6,51 +6,99 @@ import scala.util._
 
 object Sudoku {
 
-  type Square = (Int, Int)
-  type State = Either[Set[Int], Int]
-  type Board = Map[Square, State]
-  type Rule = (Board, Square) => Board
-  type Puzzle = Map[Square, Int]
+  private[this] object Private {
 
-  case object Contradiction extends Exception
+    type Square = (Int, Int)
+    type Component = Iterable[Square]
+    type State = Either[Set[Int], Int]
+    type Board = Map[Square, State]
+    type Rule = (Board, Square) => Board
 
-  val squares = for { x <- 1 to 9; y <- 1 to 9 } yield (x, y)
-  val candidates = (1 to 9).toSet
+    case object Contradiction extends Exception
 
-  @tailrec
-  def deduce(rules: Seq[Rule], board: Board): Board = {
-    val rulesApplied = rules.foldLeft(board) {
-      (board, rule) => squares.foldLeft(board)(rule)
+    val squares = for { x <- 1 to 9; y <- 1 to 9 } yield (x, y)
+    val candidates = (1 to 9).toSet
+
+    def invertedIndex[A](xss: Iterable[Iterable[A]]): Map[A, Iterable[A]] = {
+      (for (xs <- xss; x <- xs) yield (x, xs))(scala.collection.breakOut)
     }
-    if (board == rulesApplied) {
-      board
-    } else {
-      deduce(rules, rulesApplied)
-    }
-  }
 
-  def completed(board: Board) = board.forall(_._2.isRight)
+    def |*[T](ss: Iterable[Set[T]]) = ss.fold(Set.empty[T])(_ | _)
 
-  def solutions(rules: Seq[Rule], board: Board): Iterable[Board] = {
-    val deduction = deduce(rules, board)
-    if (completed(deduction)) Some(deduction)
-    else {
-      val target = deduction.minBy {
-        case (square, state) =>
-          state match {
-            case Right(_) => Integer.MAX_VALUE
-            case Left(xs) => xs.size
+    def createRule(components: Iterable[Iterable[Component]]): Rule = {
+      val invertedIndices = components.map(invertedIndex)
+      val lookupPeers: Map[Square, Component] = squares.map { square =>
+        val sets = invertedIndices.map(invertedIndex => invertedIndex(square)).map(_.toSet)
+        (square, |*(sets))
+      }(breakOut)
+      (board, square) => board(square) match {
+        case Right(d) => board
+        case Left(cs) =>
+          val knownForPeers = lookupPeers(square).flatMap(x => board(x).right.toOption).toSet
+          val possibleForMe = cs &~ knownForPeers
+          possibleForMe.size match {
+            case 0 => throw Contradiction
+            case 1 => board.updated(square, Right(possibleForMe.head))
+            case _ => board.updated(square, Left(possibleForMe))
           }
       }
-      val (square, cs) = (target._1, target._2.left.get)
-      val randomized = Random.shuffle(cs)
-      val ret = for (c <- randomized) yield {
-        val guessed = deduction.updated(square, Right(c))
-        Try(solutions(rules, guessed)).toOption
-      }
-      ret.flatten.flatten
     }
+
+    val normalRule = createRule {
+      val rows: Iterable[Component] =
+        for { x <- 1 to 9 } yield {
+          for { y <- 1 to 9 } yield (x, y)
+        }
+      val columns: Iterable[Component] =
+        for { y <- 1 to 9 } yield {
+          for { x <- 1 to 9 } yield (x, y)
+        }
+      val blocks: Iterable[Component] =
+        for { xOff <- List(0, 3, 6); yOff <- List(0, 3, 6) } yield {
+          for { x <- 1 to 3; y <- 1 to 3 } yield (x + xOff, y + yOff)
+        }
+      List(rows, columns, blocks)
+    }
+
+    // should use state monad
+    @tailrec
+    def deduce(rule: Rule, board: Board): Board = {
+      val applied = squares.foldLeft(board)(rule)
+      if (board == applied) {
+        board
+      } else {
+        deduce(rule, applied)
+      }
+    }
+
+    def completed(board: Board) = board.forall(_._2.isRight)
+
+    def solutions(rule: Rule, board: Board): Iterable[Board] = {
+      val deduction = deduce(rule, board)
+      if (completed(deduction)) Some(deduction)
+      else {
+        val target = deduction.minBy {
+          case (square, state) =>
+            state match {
+              case Right(_) => Integer.MAX_VALUE
+              case Left(xs) => xs.size
+            }
+        }
+        val (square, cs) = (target._1, target._2.left.get)
+        val randomized = Random.shuffle(cs)
+        val ret = for (c <- randomized) yield {
+          val guessed = deduction.updated(square, Right(c))
+          Try(solutions(rule, guessed)).toOption
+        }
+        ret.flatten.flatten
+      }
+    }
+
   }
+
+  import Private._
+
+  type Puzzle = Map[(Int, Int), Int]
 
   def solve(board: Puzzle): Iterable[Puzzle] = {
     val parsed: Board = squares.map { square =>
@@ -60,7 +108,7 @@ object Sudoku {
       }
       (square, state)
     }(breakOut)
-    val mySolutions = solutions(NormalSudoku.Rules, parsed)
+    val mySolutions = solutions(normalRule, parsed)
     mySolutions.map { solution =>
       solution.map { case (square, d) => (square, d.right.get) }
     }(breakOut)
