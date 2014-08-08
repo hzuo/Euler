f88fc7a26730a8a0fff42c78a0b087539ed78fc3
Howard Zuo <someotherusername@gmail.com>
2014-08-08 16:40:52 -0400

    less convoluted

--- src/main/scala/hzuo/euler/_002/Main.scala
+++ src/main/scala/hzuo/euler/_002/Main.scala
@@ -4,7 +4,7 @@ import hzuo.euler.Common._
 
 object Main extends App {
 
-  val answer = fibs().takeWhile(_ <= 4000000).filter(2 divides _.toLong).sum
+  val answer = fibs().map(_.toInt).takeWhile(_ <= 4000000).filter(2 divides _).sum
   println(answer)
 
 }
